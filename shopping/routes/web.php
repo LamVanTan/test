1df41b3f19f793fc196a006a3e0611ba70f2f1cd
nhutle25032000 <nhutle25032000@gmail.com>
2021-03-19 10:41:01 +0700

    cau truc giao dien trang admin

--- shopping/routes/web.php
+++ shopping/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.home.index');
+});
+
+Route::get('/admin',function() {
+	return view('admin.layouts.master');
 });
