e00b7093dbb4cf4870a2e97dd615af777a6ef0ce
lamvantan <vantan291@yahoo.com>
2021-03-24 11:34:21 +0700

    laravel-mix

--- shopping/resources/views/welcome.blade.php
+++ shopping/resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
+        <script type="text/javascript" src="{{ mix('/js/app.js')}}"></script>
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
         <!-- Styles -->
         <style>
             html, body {
@@ -81,7 +82,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    <b class="font-text-color">Laravel</b>
                 </div>
 
                 <div class="links">
